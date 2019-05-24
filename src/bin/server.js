0f71e1247ef3e6be979790c48bf4a9fce3ae597f
shivam-perpetualny <shivam@perpetualny.com>
2019-05-24 12:32:09 +0530

    Some configuration added for server

---
+++ src/bin/server.js
@@ -0,0 +1,71 @@
+import http from "http";
+import app from "./../app";
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = val => {
+    let port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+};
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+const onError = error => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    let bind = typeof port === "string"
+        ? "Pipe " + port
+        : "Port " + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+    case "EACCES":
+        logger.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+    case "EADDRINUSE":
+        logger.error(bind + " is already in use");
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const onListening = () => {
+    let addr = server.address();
+    let bind = typeof addr === "string"
+        ? "pipe " + addr
+        : "port " + addr.port;
+    logger.info("Listening on " + bind);
+};
+
+const port = normalizePort(nconf.get("port") || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
